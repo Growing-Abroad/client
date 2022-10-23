9761efc56ba1924d7185dac2e085a70f5f893164
Tiago Nunes <ntiagon@gmail.com>
2022-10-23 11:52:51 -0300

    storybook remove

--- src/styles/render-theme.ts
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react';
-import React from 'react';
-import { MyThemeProvider } from './MyThemeProvider';
-
-export const renderTheme = (children: React.ReactNode) => {
-  return render(<MyThemeProvider>{children}</MyThemeProvider>);
-};
