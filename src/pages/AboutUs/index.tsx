f8fdc2c0e5f35499b4b924f86631b577f38b1cfb
Augusto <ssardinhaaugusto@gmail.com>
2022-11-21 06:53:36 -0300

    feat: team members section

--- src/pages/AboutUs/index.tsx
+++ src/pages/AboutUs/index.tsx
@@ -1,9 +1,11 @@
 import WhoWeAre from "./components/WhoWeAre";
+import TeamMembers from "./components/TeamMembers";
 
 export default function AboutUs() {
   return (
     <>
       <WhoWeAre />
+      <TeamMembers />
     </>
   )
 }
