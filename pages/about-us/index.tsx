127d94f73e105a3020348359037574aa24578cc4
Augusto <ssardinhaaugusto@gmail.com>
2022-12-10 21:19:21 -0300

    fix: make about-us page appears

--- pages/about-us/index.tsx
+++ pages/about-us/index.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import HeroSection from "./components/HeroSection";
 import WhoWeAre from "./components/WhoWeAre";
 import TeamMembers from "./components/TeamMembers";
 import About from "./components/About";
+import PageLayout from "../../src/components/PageLayout";
 
 export default function AboutUs() {
   return (
@@ -12,5 +13,13 @@ export default function AboutUs() {
       <WhoWeAre />
       <TeamMembers />
     </>
+  )
+}
+
+AboutUs.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <PageLayout>
+      {page}
+    </PageLayout>
   )
 }
