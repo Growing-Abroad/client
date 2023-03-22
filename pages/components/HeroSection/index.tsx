cf64c4a34b043c574a2ca8eda54dca4fba842c00
Júnior  Nunes <denisonprobable13@gmail.com>
2023-03-22 17:50:47 -0300

    feat: Hero section finished in large screen

--- pages/components/HeroSection/index.tsx
+++ pages/components/HeroSection/index.tsx
@@ -1,18 +1,67 @@
 import React from 'react';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  Subtitle,
+  ButtonsContainer,
+  Content,
+  ImageBackground,
+} from './styles';
+import StdButton from '@/components/generics/StdButton/StdButton';
+import useAppContext from '@/hooks/useAppContext';
+import MainHeroPhoto from '@/../public/assets/main-hero-photo.webp';
 
 function HeroSection() {
+  const { isMobile } = useAppContext();
   return (
     <Container>
-      <Title.Container>
+      <Content>
         <Title.GeneralColor>
           Matching the best International{'\xa0'}
           <Title.FirstColor>TALENT{'\xa0'}</Title.FirstColor>
-          with your{'\xa0'}
-          <Title.SecondColor>COMPANY{'\xa0'}</Title.SecondColor>
+          <Title.GeneralColor>
+            with your{'\xa0'}
+            <Title.SecondColor>COMPANY{'\xa0'}</Title.SecondColor>
+          </Title.GeneralColor>
         </Title.GeneralColor>
-      </Title.Container>
+        <Subtitle>
+          Are you looking for an English Speaking Job in Germany or talents for
+          your open positions?
+        </Subtitle>
+        <ButtonsContainer>
+          <StdButton
+            style={{
+              width: `${isMobile ? '118px' : '284px'}`,
+              height: `${isMobile ? '40px' : '59px'}`,
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              padding: `${isMobile ? '8px 15px' : '20px 40px;'}`,
+              letterSpacing: '1.3px',
+              whiteSpace: 'nowrap',
+            }}
+          >
+            Find experts now
+          </StdButton>
+          <StdButton
+            style={{
+              width: `${isMobile ? '118px' : '284px'}`,
+              height: `${isMobile ? '40px' : '59px'}`,
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              padding: `${isMobile ? '8px 15px' : '20px 40px;'}`,
+              letterSpacing: '1.3px',
+              whiteSpace: 'nowrap',
+              backgroundColor: '#FFFFFF',
+            }}
+          >
+            Find A Job Now
+          </StdButton>
+        </ButtonsContainer>
+      </Content>
+      <ImageBackground background={MainHeroPhoto.src} />
     </Container>
   );
 }
--- pages/components/HeroSection/styles.ts
+++ pages/components/HeroSection/styles.ts
@@ -1,33 +1,39 @@
 import styled, { StyledProps } from 'styled-components';
 
 interface TitleProps {
-  Container: StyledProps<any>;
   GeneralColor: StyledProps<any>;
   FirstColor: StyledProps<any>;
   SecondColor: StyledProps<any>;
 }
 
+interface ImageBackgroundProps {
+  background: string;
+}
+
 export const Container = styled.div`
   min-width: 100%;
   background-color: ${({ theme }) => theme.colors.backgroundBlue};
-  padding-right: ${({ theme }) => theme.sizes.globalHorizontalPadding};
-  padding-left: ${({ theme }) => theme.sizes.globalHorizontalPadding};
+  padding: 40px ${({ theme }) => theme.sizes.globalHorizontalPadding} 0;
 `;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  position: absolute;
+  max-width: 752px;
+  z-index: 999;
+`;
 
 export const Title: TitleProps = {
-  Container: styled.div`
-    display: flex;
-    max-width: 752px;
-  `,
-  GeneralColor: styled.p`
+  GeneralColor: styled.h1`
+    display: inline-block;
+    text-align: left;
+    max-width: 100%;
     font-family: 'Montserrat';
     font-style: normal;
     font-weight: 600;
     font-size: 62px;
     line-height: 80px;
     color: ${({ theme }) => theme.colors.white};
+    text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   `,
   FirstColor: styled.span`
     color: ${({ theme }) => theme.colors.primaryYellow};
@@ -38,6 +44,45 @@ export const Title: TitleProps = {
 };
 
 export const Subtitle = styled.h2`
-  font-size: 62px;
+  /* Subheading */
+
+  width: 580px;
+  height: 114px;
+
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 38px;
+  /* or 158% */
+
+  display: flex;
+  align-items: center;
+
   color: ${({ theme }) => theme.colors.white};
+  /* Inside auto layout */
+
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 85%;
+  justify-content: space-between;
+`;
+
+export const ImageBackground = styled.div<ImageBackgroundProps>`
+  display: flex;
+  flex-wrap: wrap;
+  background-image: url(${({ background }) => background});
+  background-position: 100%;
+  background-repeat: no-repeat;
+  background-size: contain;
+  height: 450px;
+  width: 100%;
+  margin-top: 146px;
+  margin-bottom: -10px;
 `;
