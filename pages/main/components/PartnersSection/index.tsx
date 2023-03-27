f18cb26b262f3e93ffb4c48ac60973abdfd39035
Júnior  Nunes <denisonprobable13@gmail.com>
2023-03-27 16:38:00 -0300

    feat: Partners Section finished

--- pages/main/components/PartnersSection/index.tsx
+++ pages/main/components/PartnersSection/index.tsx
@@ -1,25 +1,127 @@
 import React from 'react';
 
-import { Container, Title } from './styles';
 import TwoColorTitle from '@/components/two-color-title';
-import useAppContext from '../../../../src/hooks/useAppContext';
+
+import TodayIcon from '@/../public/assets/partners-icons/today.svg';
+import HuuffpostIcon from '@/../public/assets/partners-icons/huffpost.svg';
+import TasteOfHomeIcon from '@/../public/assets/partners-icons/taste-of-home.svg';
+import ParadeIcon from '@/../public/assets/partners-icons/parade.svg';
+import RedBookIcon from '@/../public/assets/partners-icons/red-book.svg';
+import SelfIcon from '@/../public/assets/partners-icons/self.svg';
+import BuzzFeedIcon from '@/../public/assets/partners-icons/buzz-feed.svg';
+import WomansWorldIcon from '@/../public/assets/partners-icons/womans-world.svg';
+
+import {
+  Container,
+  Content,
+  ImageContainerA,
+  ImageContainerB,
+  TitleContainer,
+} from './styles';
+import Image from 'next/image';
+import useAppContext from '@/hooks/useAppContext';
 
 function PartnersSection() {
   const {
     windowSize: { width },
+    isMobile,
     smallDesktopSize,
   } = useAppContext();
 
-  const shouldBreakLine: boolean = width < smallDesktopSize;
+  const shouldBreakLine: boolean = width <= 1395;
 
   return (
     <Container>
-      <TwoColorTitle
-        text1="Some of our"
-        text2="Partners in Germany"
-        hasSpaceBtw={!shouldBreakLine}
-        breakingLine={shouldBreakLine}
-      />
+      <TitleContainer>
+        <TwoColorTitle
+          text1="Some of our"
+          text2="Partners in Germany"
+          hasSpaceBtw={!shouldBreakLine}
+          breakingLine={shouldBreakLine}
+        />
+      </TitleContainer>
+      <Content isMobile={isMobile}>
+        <ImageContainerA>
+          <Image
+            width={!isMobile ? 162 : 121}
+            height={!isMobile ? 78.34 : 58}
+            src={TodayIcon}
+            alt="Today"
+          />
+        </ImageContainerA>
+        <ImageContainerA>
+          <Image
+            width={!isMobile ? 162 : 121}
+            height={!isMobile ? 78.34 : 58}
+            src={HuuffpostIcon}
+            alt="Huffpost"
+          />
+        </ImageContainerA>
+        <ImageContainerA>
+          <Image
+            width={!isMobile ? 162 : 121}
+            height={!isMobile ? 78.34 : 58}
+            src={TasteOfHomeIcon}
+            alt="Taste of Home"
+          />
+        </ImageContainerA>
+        <ImageContainerA>
+          <Image
+            width={!isMobile ? 162 : 121}
+            height={!isMobile ? 78.34 : 58}
+            src={ParadeIcon}
+            alt="Parade"
+          />
+        </ImageContainerA>
+        <ImageContainerA>
+          <Image
+            width={!isMobile ? 162 : 121}
+            height={!isMobile ? 78.34 : 58}
+            src={RedBookIcon}
+            alt="Redbook"
+          />
+        </ImageContainerA>
+        <ImageContainerB>
+          <Image
+            width={!isMobile ? 162 : 121}
+            height={!isMobile ? 78.34 : 58}
+            src={SelfIcon}
+            alt="Self"
+          />
+        </ImageContainerB>
+        <ImageContainerB>
+          <Image
+            width={!isMobile ? 162 : 121}
+            height={!isMobile ? 78.34 : 58}
+            src={ParadeIcon}
+            alt="Parade"
+          />
+        </ImageContainerB>
+        <ImageContainerB>
+          <Image
+            width={!isMobile ? 162 : 121}
+            height={!isMobile ? 78.34 : 58}
+            src={BuzzFeedIcon}
+            alt="Buzz Feed"
+          />
+        </ImageContainerB>
+        <ImageContainerB>
+          <Image
+            width={!isMobile ? 162 : 121}
+            height={!isMobile ? 78.34 : 58}
+            src={WomansWorldIcon}
+            alt="Woman's World"
+          />
+        </ImageContainerB>
+        <ImageContainerB>
+          <Image
+            width={!isMobile ? 162 : 121}
+            height={!isMobile ? 78.34 : 58}
+            src={HuuffpostIcon}
+            alt="Huffpost"
+          />
+        </ImageContainerB>
+      </Content>
     </Container>
   );
 }
--- pages/main/components/PartnersSection/styles.ts
+++ pages/main/components/PartnersSection/styles.ts
@@ -1,5 +1,61 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div``;
+interface ContentProps {
+  isMobile: boolean;
+}
 
-export const Title = styled.h1``;
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 140px;
+
+  ${({ theme }) => css`
+    @media (max-width: ${theme.sizes.mediaQuery}) {
+      padding: 0 30px;
+    }
+  `}
+`;
+
+export const TitleContainer = styled.div`
+  padding: 0 80px;
+`;
+
+export const ImageContainerA = styled.div`
+  display: grid;
+  grid-area: 'AreaA';
+`;
+
+export const ImageContainerB = styled.div`
+  display: grid;
+  grid-area: 'AreaB';
+`;
+
+export const Content = styled.div<ContentProps>`
+  display: grid;
+  width: 100%;
+  max-width: 100%;
+  margin-top: 60px;
+  grid-gap: 24px;
+  justify-content: center;
+  align-items: center;
+
+  ${({ isMobile }) =>
+    isMobile
+      ? css`
+          grid-template-areas:
+            'AreaA AreaA'
+            'AreaA AreaA'
+            'AreaA AreaA'
+            'AreaB AreaB'
+            'AreaB AreaB'
+            'AreaB AreaB';
+          /* grid-gap: 5px; */
+        `
+      : css`
+          grid-template-areas:
+            'AreaA AreaA AreaA AreaA AreaA'
+            'AreaB AreaB AreaB AreaB AreaB';
+        `}
+`;
