4e51a07569488cd4589b9603d350f663ba3a6054
trettin <gtcastro7@gmail.com>
2023-03-27 19:31:24 -0300

    fixing coaching page

--- pages/coaching/components/hero-section/index.tsx
+++ pages/coaching/components/hero-section/index.tsx
@@ -28,16 +28,24 @@ export default function CoachingHeroSection() {
             <ContainerHero 
             backgroundImageDesktop={CoachingHeroBg.src}  
             backgroundImageMobile={CoachingHeroBg.src}
-            styles={isMobile ? {height: '100%', justifyContent: 'flex-start', backgroundPosition: '-155px'} : {height: '100%', justifyContent: 'flex-start'}}
+            styles={isMobile ? {
+                height: '100%', 
+                justifyContent: 'flex-start', 
+                backgroundPosition: '-155px', 
+            } 
+            : {
+                height: '100%', 
+                justifyContent: 'flex-start',
+            }}
             >
                 <HeroTitleWrapper>
                     <TwoColorTitle 
                     text1="1:1" 
                     text2="Coaching" 
                     hasSpaceBtw 
-                    wrapperStyles={isMobile ? {padding: `0 0 14px`, flexDirection: 'column'} : {padding: `0 0 35px`}} 
+                    wrapperStyles={isMobile ? {padding: `0 0 14px`} : {padding: `0 0 35px`}} 
                     styles={isMobile 
-                        ? {marginBottom: '0', fontSize: '44px'} 
+                        ? {marginBottom: '0', fontSize: '44px', flexDirection: 'column', display: 'flex'} 
                         : {marginBottom: '0'}
                     }
                     />
