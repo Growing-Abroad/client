57bbd69efce94ffcf0e73cb57461e9094eaeaf32
Júnior  Nunes <denisonprobable13@gmail.com>
2023-04-11 15:27:41 -0300

    fix: header for companies working too

--- pages/candidates/index.tsx
+++ pages/candidates/index.tsx
@@ -1,16 +1,16 @@
-import CallToAction from './components/CallToAction/CallToAction';
-import WorkIn from './components/WorkIn/WorkIn';
-import OurServicesSection from './components/OurServices';
-import SuccessStories from './components/Success-stories';
-import CommunitySection from './components/Community';
-import BlogSection from './components/Blog/BlogSection';
-import { ReactElement } from 'react';
-import { NextPageWithLayout } from '@pages/_app';
-import PageLayout from '@components/PageLayout';
-import DiscoverGA from './components/DiscoverGA';
-import { IntlProvider } from 'react-intl';
-import { ChosenHeader } from '@/components/PageLayout/PageLayout';
-import _useLocale from '@/hooks/useLocale';
+import CallToAction from "./components/CallToAction/CallToAction";
+import WorkIn from "./components/WorkIn/WorkIn";
+import OurServicesSection from "./components/OurServices";
+import SuccessStories from "./components/Success-stories";
+import CommunitySection from "./components/Community";
+import BlogSection from "./components/Blog/BlogSection";
+import { ReactElement } from "react";
+import { NextPageWithLayout } from "@pages/_app";
+import PageLayout from "@components/PageLayout";
+import DiscoverGA from "./components/DiscoverGA";
+import { IntlProvider } from "react-intl";
+import { ChosenHeader } from "@/components/PageLayout/PageLayout";
+import _useLocale from "@/hooks/useLocale";
 
 const Page: NextPageWithLayout = () => {
   return (
@@ -31,7 +31,7 @@ Page.getLayout = function getLayout(page: ReactElement) {
 
   return (
     <IntlProvider locale={locale!} messages={messages}>
-      <PageLayout chosenHeader={ChosenHeader.FOR_CANDIDATES}>{page}</PageLayout>
+      <PageLayout chosenHeader={ChosenHeader.FOR_COMPANIES}>{page}</PageLayout>
     </IntlProvider>
   );
 };
