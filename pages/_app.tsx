82242f9fe5d44e370b58456b90e5df65bca6b385
trettin <gtcastro7@gmail.com>
2023-04-11 23:27:58 -0300

    deleting commented code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,16 +28,7 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
       {useWithAuth() && (
         <MyThemeProvider>
           {getLayout(
-            <>
-
-            {/* <style>{`
-              @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap');
-              body {
-                font-family: 'Montserrat', sans-serif;
-              }
-            `}</style> */}
               <Component {...pageProps} />
-            </>,
           )}
         </MyThemeProvider>)
       }
