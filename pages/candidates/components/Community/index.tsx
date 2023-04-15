b6b171549f4fb755870caea59fb54d09eb7aaca1
trettin <gtcastro7@gmail.com>
2023-04-15 19:20:49 -0300

    update

--- pages/candidates/components/Community/index.tsx
+++ pages/candidates/components/Community/index.tsx
@@ -19,6 +19,7 @@ function CommunitySection() {
       window.innerHeight || document.documentElement.clientHeight;
     const windowWidth =
       window.innerWidth || document.documentElement.clientWidth;
+    if (!rect) return;
     const vertInView = rect.top <= windowHeight && rect.top + rect.height >= 0;
     const horInView = rect.left <= windowWidth && rect.left + rect.width >= 0;
     return vertInView && horInView;
