14db43978377ce2b5777ea8eabd1dd24fcce1308
trettin <gtcastro7@gmail.com>
2023-04-17 22:01:09 -0300

    saving progress in coaching page

--- pages/about-us/components/VisionAndMissionNew/index.tsx
+++ pages/about-us/components/VisionAndMissionNew/index.tsx
@@ -1,36 +1,42 @@
 import React from "react"
 import { Container, Row, Col } from "react-bootstrap"
-// import VisionAndMissionBackground from "../../../../public/assets/puzzle.webp"
-import VisionAndMissionBackground from "../../../../public/assets/pages/coaching/career-coaches.webp"
-
 import classes from "./style.module.css"
 
-function VisionAndMissionNew() {
+export interface IText {
+  heading: string,
+  description: string
+}
+export interface IVisionAndMission {
+  text1: IText,
+  text2: IText,
+  backgroundSrc: string
+}
+
+function VisionAndMissionNew({backgroundSrc, text1, text2}: IVisionAndMission) {
   return (
     <>
       <Container className={classes.sectionContainer}>
         <Row className={classes.section}>
-          <Col lg="6" className={classes.missionBg} style={{ backgroundImage: 'url(' + VisionAndMissionBackground.src + ')', backgroundPosition: 'left' }}>
+          <Col lg="6" className={classes.missionBg} style={{ backgroundImage: 'url(' + backgroundSrc + ')' }}>
             <div className={classes.missionContainer}>
               <div className={classes.missionInner}>
-                <h3>Goal</h3>
+                <h3>{text1.heading}</h3>
                 <p>
-                  With our Coaching session you will be perfectly prepared for the german market and will have the best chances to land your dream job
+                  {text1.description}
                 </p>
               </div>
             </div>
           </Col>
           <Col lg="6" className={classes.visionCol}>
             <div className={classes.visionContainer}>
-              <h3>Experts</h3>
+              <h3>{text2.heading}</h3>
               <p>
-                All your Coaches are proven experts from Germany and will guide you through every step of your journey to land your dream job abraod.
+                {text2.description}
               </p>
             </div>
           </Col>
         </Row>
       </Container>
-
     </>
   )
 }
