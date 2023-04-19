3d61ec9209a5a3877f34e56e1d401e0cec9ca9d6
Júnior  Nunes <denisonprobable13@gmail.com>
2023-04-19 18:13:43 -0300

    consulting section texts changed

--- src/utils/mock-ups/consulting-gmbh-mock-texts.ts
+++ src/utils/mock-ups/consulting-gmbh-mock-texts.ts
@@ -1,25 +1,23 @@
-import Img1 from '@/../public/assets/Photos-Main-Page/GrowingAbroadUanBusiness_Canva.png';
-import Img2 from '@/../public/assets/Photos-Main-Page/high-five.webp';
-import Img3 from '@/../public/assets/Photos-Main-Page/Manu-Business_Canva.webp';
+import Img1 from "@/../public/assets/Photos-Main-Page/GrowingAbroadUanBusiness_Canva.png";
+import Img2 from "@/../public/assets/Photos-Main-Page/high-five.webp";
+import Img3 from "@/../public/assets/Photos-Main-Page/Manu-Business_Canva.webp";
 
 export default [
   {
     imgSrc: Img1,
-    title: 'Our Industry Expertise:',
-    from: ``,
+    title: "Uanderson Silva:",
+    from: `Co-founder`,
     texts: [
-      `Financial Service,`,
-      `Management and Consulting,`,
-      `Tech,`,
-      `Automotive`,
-      `and Industry 4.0`,
+      `MSc + MBA in German University`,
+      `Ex Bosch, Accenture, Allianz,`,
+      `Management Experience`,
     ],
-    href: '',
-    countryFlag: 'br',
+    href: "",
+    countryFlag: "br",
   },
   {
     imgSrc: Img2,
-    title: 'Our Industry Expertise:',
+    title: "Our Industry Expertise:",
     from: ``,
     texts: [
       `Financial Service,`,
@@ -28,21 +26,19 @@ export default [
       `Automotive`,
       `and Industry 4.0`,
     ],
-    href: '',
-    countryFlag: 'de',
+    href: "",
+    countryFlag: "de",
   },
   {
     imgSrc: Img3,
-    title: 'Our Industry Expertise:',
-    from: ``,
+    title: "Manuel Feneberg:",
+    from: `Co-founder`,
     texts: [
-      `Financial Service,`,
-      `Management and Consulting,`,
-      `Tech,`,
-      `Automotive`,
-      `and Industry 4.0`,
+      `Management & University Trainer`,
+      `Certified Professional Coach`,
+      `Professional Actor`,
     ],
-    href: '',
-    countryFlag: 'us',
+    href: "",
+    countryFlag: "us",
   },
 ];
