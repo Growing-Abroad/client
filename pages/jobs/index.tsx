b470e3bdf0c729254d3c1c7f3fa1eb66c2f78d50
trettin <gtcastro7@gmail.com>
2023-06-12 20:13:32 -0300

    saving working in progress on jobs page

--- pages/jobs/index.tsx
+++ pages/jobs/index.tsx
@@ -4,13 +4,34 @@ import CloudSection from "./components/cloud-section";
 import JobsHeroSection from "./components/jobs-hero-section";
 import UploadCvSection from "./components/upload-cv-section";
 import { ChosenHeader } from "@/components/PageLayout/PageLayout";
+import PageBodyLayout from "@/components/generics/PageBody";
+import useAppContext from "@/hooks/useAppContext";
+import { EWaveType } from "@/components/Wave";
 
 
 export default function Jobs() {
+    const {windowSize: {height}} = useAppContext();
+
+    function getDistanceFromTop() {
+        if (height > 1513) {
+            return '628px';
+        }
+        if (height > 430) {
+            return 'calc(25.75rem + ((1vw - 4.3px) * 19.9446))';
+        }
+        return '412px;'
+    }
+
     return <>
         <JobsHeroSection />
-        <CloudSection />
-        <UploadCvSection />
+        <PageBodyLayout 
+            distanceFromTop={'100px'} 
+            id="page-jobs"
+            waveType={EWaveType.linear}
+        >
+            <CloudSection />
+            <UploadCvSection />
+        </PageBodyLayout>
     </>
 }
 
