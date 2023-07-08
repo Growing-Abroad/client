03dd4f948317fe4b2ef30a0e059289576b6f5655
trettin <gtcastro7@gmail.com>
2023-07-08 10:01:56 -0300

    changing auth logic

--- src/components/DevAuth/index.tsx
+++ src/components/DevAuth/index.tsx
@@ -5,11 +5,8 @@ const useWithAuth = () => {
     const { password } = router.query;
 
     if (
-        !process.env.NEXT_PUBLIC_AUTH || 
-        (process.env.NODE_ENV === 'production' 
-        && process.env.NEXT_PUBLIC_AUTH !== password)
+        process.env.NEXT_PUBLIC_AUTH && process.env.NEXT_PUBLIC_AUTH !== password
     ) {
-        // In production, require password to match environment variable
         return false;
     }
 
