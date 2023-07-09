ce3d7c3f21f5540d7628f2202ec8efb43d032ac8
LuCaSLiMa12 <dev.lucaslima@outlook.com>
2023-07-09 13:12:52 -0300

    feat: redirect home to candidates page

--- pages/candidates/components/Blog/BlogCard/index.tsx
+++ pages/candidates/components/Blog/BlogCard/index.tsx
@@ -1,12 +1,9 @@
-// import { useNavigate } from 'react-router-dom'
 import * as S from "@/styles/pages-styles/candidates/Blog/BlogCard/index.styles";
 import { IPost } from "@utils/blog_posts";
 
 export default function BlogCard({ id, title, category, image }: IPost) {
-  // const navigate = useNavigate()
 
   const handleRedirectToBlogPost = (id: string) => {
-    // navigate('blog/post/' + id)
     return id;
   };
 
